be58dca74545bc339a8df7fe4d60326c556214ca
ArimoUg <92304384+ArimoUg@users.noreply.github.com>
2022-11-29 11:28:41 +0200

    Create Toiminnot.js

---
+++ Toiminnot.js
@@ -0,0 +1,69 @@
+const kirjoitus = document.querySelector('.kirjoitusbox')
+const nappi = document.querySelector('.lisaanappi')
+const lista = document.querySelector('.todolista')
+
+function painaNappia(e) {
+    e.preventDefault()
+    addTodo()
+}
+
+// Listätään To Do listaan
+$("#btn2").click(function(){
+    $("ol").append("<li>Appended item</li>");
+  });
+});
+function addTodo() {
+    const kaikkipalat = document.createElement('div')
+    kaikkipalat.classList.add('kaikkipalat')
+
+    const pala = document.createElement('p')
+    pala.classList.add('pala')
+    pala.innerText = kirjoitus.value
+    kaikkipalat.appendChild(pala)
+
+    // Tarkistetaan onko tyhjä ja annetaan varoitus
+    if (kirjoitus.value === '') {
+        alert("Tekstikenttä on tyhjä! Et voi lisätä listausta.")
+    } else {
+
+    const valmisbutton = document.createElement("button")
+    valmisbutton.innerHTML = '<i class="buttontwo"></i>'
+    valmisbutton.classList.add("valmis-button")
+    kaikkipalat.appendChild(valmisbutton)
+
+    const roskabutton = document.createElement("button")
+    roskabutton.innerHTML = '<i class="buttontwo"></i>'
+    roskabutton.classList.add("roska-button")
+    kaikkipalat.appendChild(roskabutton)
+
+    lista.appendChild(kaikkipalat)
+    kirjoitus.value = ''
+    }
+}
+
+// Valmis ja poista toiminnot
+function merkkaa(e) {
+    const pala = e.target
+
+    // Poista
+    if (pala.classList[0] === 'roska-button') {
+        const todolist = pala.parentElement
+        todolist.remove()
+    }
+	
+	//Poista kaikki
+	if (pala.classLisst[0] === 'poistak-button')
+    
+	// Valmis
+    if (pala.classList[0] === 'valmis-button') {
+        const todolist = pala.parentElement
+        todolist.classList.toggle('valmis')
+    }
+	
+	//Valmis kaikki
+	if (pala.classList[0] === 'valmisk-button')
+    
+}
+
+nappi.addEventListener('click', painaNappia)
+lista.addEventListener('click', merkkaa)
